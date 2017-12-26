5559a3fb7497ee8d1d918409a1d0299f403a0e72
Sebastian Schepens <sebastian.schepens@mercadolibre.com>
2017-12-26 16:47:19 -0300

    Tweaks

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -15,21 +15,18 @@ function fish_prompt
   set -l fish     "⋊>"
   set -l ahead    "↑"
   set -l behind   "↓"
-  set -l diverged "⥄ "
-  set -l dirty    "⨯"
-  set -l none     "◦"
+  set -l diverged "⇅"
+  set -l dirty    "±"
+  set -l none     "="
+  set -l prompt_end "─╼"
+  set -l no_upstream "⤉"
 
   set -l normal_color     (set_color normal)
   set -l success_color    (set_color $fish_pager_color_progress ^/dev/null; or set_color cyan)
   set -l error_color      (set_color $fish_color_error ^/dev/null; or set_color red --bold)
+  set -l warning_color    (set_color $fish_color_quote ^/dev/null; or set_color brown)
   set -l directory_color  (set_color $fish_color_quote ^/dev/null; or set_color brown)
   set -l repository_color (set_color $fish_color_cwd ^/dev/null; or set_color green)
-
-  if test $last_command_status -eq 0
-    echo -n -s $success_color $fish $normal_color
-  else
-    echo -n -s $error_color $fish $normal_color
-  end
 
   if git_is_repo
     if test "$theme_short_path" = 'yes'
@@ -42,12 +39,25 @@ function fish_prompt
     echo -n -s " on " $repository_color (git_branch_name) $normal_color " "
 
     if git_is_touched
-      echo -n -s $dirty
+      echo -n -s $error_color $dirty $normal_color
     else
-      echo -n -s (git_ahead $ahead $behind $diverged $none)
+      set -l git_status (git_ahead $ahead $behind $diverged $none)
+      if test "$git_status" = ""
+        echo -n -s $warning_color $no_upstream $normal_color
+      else if test "$git_status" = "$none"
+        echo -n -s $git_status
+      else
+        echo -n -s $warning_color $git_status $normal_color
+      end
     end
   else
     echo -n -s " " $directory_color $cwd $normal_color
+  end
+
+  if test $last_command_status -eq 0
+    echo -n -s " " $success_color $prompt_end $normal_color
+  else
+    echo -n -s " " $error_color $prompt_end $normal_color
   end
 
   echo -n -s " "
